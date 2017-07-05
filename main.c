6f8e7d9e2cf68017efb8d47b872fa386272de8d3
Thiago <thiagolamf@gmail.com>
2017-07-04 23:17:25 -0300

    Exercicio 1 completo

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 
 
 // DEFINES:
-#DEFINE N_ENTRADAS 31
-#DEFINE N_EPOCAS 2
+#define N_ENTRADAS 31
+#define N_EPOCAS 2
 
-#DEFINE FLAG_ALEATORIO 1
+#define FLAG_ALEATORIO 1
 
 typedef struct neuro
 {
@@ -15,35 +15,11 @@ typedef struct neuro
     float entrada;
 }Neuronio;
 
-
-
-int main()
+typedef struct entrada
 {
-    srand(time(NULL));
-
-    char *arquivos = {"treinamento.txt","teste.txt"};
-    //inicia neuronio
-    Neuronio n;
-    iniciaPesos(&n,FLAG_ALEATORIO);
-
-    //inicia treinamento
-    int *entradaTreinamento = leArquivo(arquivos[0]);
-
-    int resultado;
-    int flag0 = 0, flag1 = 0;
-    int epoch =0;
-    while(!flag0 && !flag1)
-    {
-        //treina 0:
-        flag0 = calculaNeuronio(n,entradaTreinamento);
-
-        //treina 1:
-        flag1 = calculaNeuronio(n,entradaTreinamento);
-        epoch++;
-    }
-
-
-}
+    int valores[N_ENTRADAS];
+    int numero;// -1 = indefinido
+}Entrada;
 
 /**
     Inicia pesos aleatorios ou zerados
@@ -69,27 +45,59 @@ void iniciaPesos(Neuronio *neuronios,int flagAleatorio)
 
 void imprimePesos(Neuronio *n)
 {
+    printf("\nPESOS:\n");
     int i;
     for(i=1;i<N_ENTRADAS;i++)
     {
-        printf("%f\t");
+        printf("%f\t",n->peso[i]);
         if(i % 3 == 0) printf("\n");
+    }
+}
+void imprimeEntradas(Entrada *entradas,int totalEntradas)
+{
+    int i;
+    int j;
+    printf("\nENTRADAS:\n");
+    for(i=0;i<totalEntradas;i++)
+    {
+        for(j=1;j<N_ENTRADAS;j++)
+        {
+            printf("%d",entradas[i].valores[j]);
+            if(j % 5 == 0) printf("\n");
+        }
+        printf("\n\n");
     }
 }
 /**
     Lê entradas no arquivo
 **/
-int *leArquivo(char *arquivo)
+Entrada *leArquivo(char *arquivo,int totalEntradasArquivo)
 {
     FILE *f;
 
     f = fopen(arquivo,"r");
 
     int i;
-    int *entradas = (int*) malloc(sizeof(int)*N_ENTRADAS);
-    for(i=0;i<N_ENTRADAS;i++)
+    int j;
+    char corrente;
+    Entrada *entradas = (Entrada*) malloc(sizeof(Entrada)*totalEntradasArquivo);
+    //Entrada entradas[totalEntradasArquivo];
+    //int *entradas = (int*) malloc(sizeof(int)*N_ENTRADAS);
+    for(i=0;i<totalEntradasArquivo;i++)
     {
-        fscanf(f,"%d",entradas[i]);
+        j = 0;
+        while(j<N_ENTRADAS)
+        {
+            corrente = fgetc(f);
+            if(corrente != '0' && corrente != '1')
+            {
+                continue;
+            }
+            //fscanf(f,"%c",&entradas[i].valores[j]);
+            entradas[i].valores[j] = corrente - '0';
+            //printf( " %d",entradas[i].valores[j]);
+            j++;
+        }
     }
 
     fclose(f);
@@ -104,13 +112,13 @@ int calculaNeuronio(Neuronio * neuro, int *entrada)
     int i;
     float resultado = 0;
 
-    for(i=0;i<N_ENTRADAS;i++)
+    for(i=1;i<N_ENTRADAS;i++)
     {
         resultado += neuro->peso[i] * entrada[i];
     }
 
     //Função degrau:
-    if(resultado >= 1)
+    if(resultado > 0)
     {
         return 1;
     }
@@ -122,8 +130,73 @@ void calculaPeso(Neuronio * neuro, int *entrada,float e)
     int i;
 
     neuro->entrada = e;
-    for(i=0;i<N_ENTRADAS;i++)
+    for(i=1;i<N_ENTRADAS;i++)
     {
-        neuro->peso[i] += neuro->entrada * entrada[i];
+        neuro->peso[i] += neuro->entrada * 1 * entrada[i];
+        //printf("\nNovo Peso: %f",neuro->peso[i]);
     }
+}
+
+
+
+int main()
+{
+    srand(time(NULL));
+
+    //inicia neuronio
+    Neuronio *n = (Neuronio*)malloc(sizeof(Neuronio));
+    iniciaPesos(n,FLAG_ALEATORIO);
+
+    Entrada **entradas = (Entrada**) malloc(sizeof(Entrada*)*6);
+    entradas[0] = leArquivo("0.txt",10);
+    entradas[1] = leArquivo("1.txt",10);
+    entradas[2] = leArquivo("2.txt",1);
+    entradas[3] = leArquivo("3.txt",1);
+    entradas[4] = leArquivo("4.txt",1);
+    entradas[5] = leArquivo("5.txt",1);
+    //Entrada *entradasZero = leArquivo("0.txt",10);
+    //Entrada *entradasUm = leArquivo("1.txt",10);
+    //imprimeEntradas(entradas[0],10);
+
+    //inicia treinamento
+    int flag0 = 0, flag1 = 0;
+    int epoch =0;
+    while(!flag0 || !flag1)
+    {
+        printf("\nEPOCA: %d",epoch);
+        //treina 0:
+        flag0 = 0;
+        flag1 = 0;
+        int flag;
+        flag = calculaNeuronio(n,entradas[0][0].valores);
+        calculaPeso(n,entradas[0][0].valores,(0-flag)); //desejado - obtido
+        if(flag == 0) flag0 = 1;
+
+        //treina 1:
+        flag = calculaNeuronio(n,entradas[1][0].valores);
+        calculaPeso(n,entradas[1][0].valores,(1-flag));
+        if(flag == 1) flag1= 1;
+        epoch++;
+    }
+    imprimePesos(n);
+
+    //Testa 0s
+    int i;
+    for(i=0;i<10;i++)
+    {
+        printf("TESTE0:%d\n",calculaNeuronio(n,entradas[0][i].valores));
+    }
+    //Testa 1s
+    for(i=0;i<10;i++)
+    {
+        printf("TESTE1:%d\n",calculaNeuronio(n,entradas[1][i].valores));
+    }
+    printf("TESTE2:%d\n",calculaNeuronio(n,entradas[2][0].valores));
+    printf("TESTE3:%d\n",calculaNeuronio(n,entradas[3][0].valores));
+    printf("TESTE4:%d\n",calculaNeuronio(n,entradas[4][0].valores));
+    printf("TESTE5:%d\n",calculaNeuronio(n,entradas[5][0].valores));
+    //Testa demais
+
+
+    return 0;
 }
